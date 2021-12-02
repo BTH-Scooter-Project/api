ebfed0f7f619e0496675b81fcb6ee6c5d562b4e4
gurkan444 <gusu20@student.bth.se>
2021-12-02 15:43:00 +0100

    Merged "databas"-repo into this repo

--- routes/city.js
+++ routes/city.js
@@ -6,6 +6,10 @@ const cityModel = require("../models/city.js");
 //route /v1/city
 router.get('/', (req, res) => cityModel.getAllCities(res, req));
 
-//route /v1/hello
+//route /v1/city/:id
+router.get('/:id', (req, res) => cityModel.getCityById(res, req));
+
+//route /v1/city/:id/station
+
 
 module.exports = router;
