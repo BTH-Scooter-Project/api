d44f0c610d252bec4c8e3482a122dd5235c90b98
emeu17 <emeu17@student.bth.se>
2021-12-16 20:58:55 +0000

    added route to retrieve stations+bikes in spec. city

--- models/city.js
+++ models/city.js
@@ -32,6 +32,103 @@ const city = {
         });
     },
     /*
+        get all stations and bikes in city with id id
+    */
+    getCityById: function (res, req) {
+        let db;
+
+        db = database.getDb();
+
+        let cityId = req.params.id;
+
+        var sqlBike = `SELECT * from BIKE
+                        WHERE cityid = ?;`;
+        var paramsBike = [cityId];
+
+        //get all bikes in city with id = id
+        let getBikes = function(callback) {
+            db.all(sqlBike, paramsBike, function(err, rows) {
+                callback(err, rows);
+            });
+        };
+
+        //get bike information (callback to be able to use it further down)
+        //and use it together with station info
+        getBikes(function(err, bikes) {
+            if (err) {
+                return res.status(500).json({
+                    errors: {
+                        status: 500,
+                        source: `/v1/city${req.path}`,
+                        message: "Error retrieving bike information",
+                        detail: err.message
+                    }
+                });
+            }
+
+            return city.getCityStations(res, req, bikes, cityId, db);
+        });
+    },
+    getCityStations: function (res, req, bikes, cityId, db) {
+        var sqlStation = `SELECT * from STATION
+                        WHERE cityid = ? or cityid = ?;`;
+        var paramsStation = [cityId, -1];
+
+        let cityInfo = [];
+        let i = 0;
+
+        db.all(sqlStation, paramsStation, (error, rows) => {
+            if (error) {
+                return res.status(500).json({
+                    errors: {
+                        status: 500,
+                        source: `/v1/city${req.path}`,
+                        message: "Error retrieving station information",
+                        detail: error.message
+                    }
+                });
+            }
+
+            //if rows only contain one station it is -1 and
+            //if city also has no bikes
+            //then the city in question does not exist
+            if (rows.length <= 1 && bikes.length == 0) {
+                return res.status(400).json({
+                    errors: {
+                        status: 400,
+                        source: `/v1/city${req.path}`,
+                        message: "No such city",
+                        detail: "No city with that id exists"
+                    }
+                });
+            }
+
+            //for each station, add the corresponding bikes
+            rows.forEach((row) => {
+                let newObj = {
+                    stationid: row.stationid,
+                    type: row.type,
+                    address: row.address,
+                    gps_lat: row.gps_lat,
+                    gps_lon: row.gps_lon,
+                    bikes: []
+                };
+
+                cityInfo[i] = newObj;
+
+                var stationBikes = bikes.find(bike => bike.stationid == row.stationid);
+
+                cityInfo[i].bikes.push(stationBikes);
+
+                i++;
+            });
+
+            return res.status(200).json({
+                data: cityInfo
+            });
+        });
+    },
+    /*
         get all stations in city with id cityId
     */
     getStations: function (res, req) {
